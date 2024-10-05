be91b8d7b872ffe62c5e655bbf2875e432cd456f
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-10-05 18:11:41 +0300

    Updated www folder

--- www/script.js
+++ www/script.js
@@ -1,15 +1,38 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
 
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
 });
 
-  window.onscroll = function() {
-    const header = document.querySelector('header');
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      header.classList.add('scrolled'); // Add class when scrolled down
-    } else {
-      header.classList.remove('scrolled'); // Remove class when at the top
-    }
-  };
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("scrolled"); // Add class when scrolled down
+  } else {
+    header.classList.remove("scrolled"); // Remove class when at the top
+  }
+};
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
