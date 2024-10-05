e0212165fef2daebe3f5cec9d750555524600025
quiesscent <ciphedc@gmail.com>
2024-10-05 21:56:21 +0300

    Public Folder Set Up & Data Display

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,27 +2,31 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-
 import * as middlewares from './middlewares';
-import api from './api';
+import apiRouter from './api';
 import MessageResponse from './interfaces/MessageResponse';
+import path from 'path';
 
+// import index
 require('dotenv').config();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+app.get<{}, any>('/', (req, res) => {
+  // res.json({message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",});
+
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use('/api/v1', api);
+
+// app.use('/api/v1', api);
+app.use('/api', apiRouter);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
